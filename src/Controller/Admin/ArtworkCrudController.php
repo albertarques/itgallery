b33d5deaca18862b1ba176cfd1776db182047326
Albert Arques <albertarques@gmail.com>
2024-05-17 10:32:37 +0200

    Update ArtworkCrudController and template

--- src/Controller/Admin/ArtworkCrudController.php
+++ src/Controller/Admin/ArtworkCrudController.php
@@ -3,15 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Artwork;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\FormField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use Vich\UploaderBundle\Form\Type\VichImageType;
-use Vich\UploaderBundle\VichUploaderBundle;
 
 class ArtworkCrudController extends AbstractCrudController
 {
@@ -20,45 +21,88 @@ class ArtworkCrudController extends AbstractCrudController
         return Artwork::class;
     }
 
-
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud::new()
+            ->overrideTemplates([
+                'crud/new' => 'admin/artwork_new.html.twig'
+            ])
+        ;
+    }
     public function configureFields(string $pageName): iterable
     {
-        return [
-            ImageField::new('image', 'Image')
-                ->setUploadDir('public/uploads/artworks'),
-//                ->setBasePath('public/uploads/artworks')
-//                ->setUploadDir('public/uploads/artworks'),
-            TextField::new('language'),
+        $image = ImageField::new('image', 'Image')
+            ->setUploadDir('public/uploads/artworks')
+            ->setBasePath('uploads/artworks');
+        $language = TextField::new('language');
+        $title = TextField::new('name', 'title');
+        $series_name = TextField::new('series_name');
+        $artist = TextField::new('artist');
+        $year = NumberField::new('year');
+        $inventory_id = TextField::new('inventory_id');
+        $status = TextField::new('status');
+        $availability = TextField::new('availability');
+        $ownership = TextField::new('ownership');
+        $tags = ArrayField::new('tags');
+        $dimensions = ArrayField::new('dimensions');
+        $variable_dimensions = BooleanField::new('variable_dimensions');
+        $duration = ArrayField::new('duration');
+        $weight = NumberField::new('weight');
+        $edition = NumberField::new('edition');
+        $unique_edition = BooleanField::new('unique_edition');
+        $price = ArrayField::new('price');
+        $no_price = BooleanField::new('no_price');
+        $artist_amount = NumberField::new('artist_amount');
+        $artist_amount_type = TextField::new('artist_amount_type');
+        $description = TextEditorField::new('description');
+        $type = TextField::new('type');
+        $support = TextField::new('support');
+        $material = TextField::new('material');
+        $location = TextField::new('location');
+        $sublocation = TextField::new('sublocation');
+        $notes = TextField::new('notes');
+        $documentation_notes = TextEditorField::new('documentation_notes');
+        $pdf_notes = TextField::new('pdf_notes');
 
-            TextField::new('name', 'title'),
-            TextField::new('series_name'),
-            TextField::new('artist'),
-            NumberField::new('year'),
-            TextField::new('inventory_id'),
-            TextField::new('status'),
-            TextField::new('availability'),
-            TextField::new('ownership'),
-            ArrayField::new('tags'),
-            ArrayField::new('dimensions'),
-            BooleanField::new('variable_dimensions'),
-            ArrayField::new('duration'),
-            NumberField::new('duration'),
-            NumberField::new('weight'),
-            NumberField::new('edition'),
-            BooleanField::new('unique_edition'),
-            ArrayField::new('price'),
-            BooleanField::new('no_price'),
-            NumberField::new('artist_amount'),
-            TextField::new('artist_amount_type'),
-            TextEditorField::new('description'),
-            TextField::new('type'),
-            TextField::new('support'),
-            TextField::new('material'),
-            TextField::new('location'),
-            TextField::new('sublocation'),
-            TextField::new('notes'),
-            TextEditorField::new('documentation_notes'),
-            TextField::new('pdf_notes')
+        if (Crud::PAGE_INDEX === $pageName) {
+            return [
+                $image,
+                $title,
+                $artist
+            ];
+        }
+
+        return [
+            FormField::addColumn(4),
+            $language,
+            $image,
+            $title,
+            $series_name,
+            $year,
+            $inventory_id,
+            $status,
+            $availability,
+            $ownership,
+            $tags,
+            $dimensions,
+            $variable_dimensions,
+            $duration,
+            $weight,
+            $edition,
+            $unique_edition,
+            $price,
+            $no_price,
+            $artist_amount,
+            $artist_amount_type,
+            $description,
+            $type,
+            $support,
+            $material,
+            $location,
+            $sublocation,
+            $notes,
+            $documentation_notes,
+            $pdf_notes
         ];
     }
 }
